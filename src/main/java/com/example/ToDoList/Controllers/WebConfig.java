fa828103e52a45caecbd107ffab690e603fa60e3
Giridhar Neelam <giridharneelam17@gmail.com>
2025-10-24 20:04:35 +0530

    Update WebConfig.java

--- src/main/java/com/example/ToDoList/Controllers/WebConfig.java
+++ src/main/java/com/example/ToDoList/Controllers/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://ecom-frontend-react.vercel.app"
+                        "https://to-do-list-app-nine-phi.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
